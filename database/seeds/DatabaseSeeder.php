1fecba0bfd08c05a1b03c9f2360425786e835095
Sand Of Vega <sandofvega@gmail.com>
2018-08-27 12:02:43 +0600

    create Models folder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(ProductionSeeder::class);
+//         $this->call(UsersTableSeeder::class);
     }
 }
