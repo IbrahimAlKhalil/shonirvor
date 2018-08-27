64ad3c5dfc71b7d17dd1745cd4410c36f003e5a9
Sand Of Vega <sandofvega@gmail.com>
2018-08-27 16:26:58 +0600

    add debugbar

---
+++ app/Http/Controllers/Backend/DealerController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DealerController extends Controller
+{
+    public function __construct()
+    {
+//        $this->middleware('role:superadmin');
+    }
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(User $user)
+    {
+        //
+    }
+
+
+    public function edit(User $user)
+    {
+        //
+    }
+
+
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+
+    public function destroy(User $user)
+    {
+        //
+    }
+}
