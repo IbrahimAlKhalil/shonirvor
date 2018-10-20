5eb708ff9dfcbcbfcb9c16c403e35fbcd26ec9fd
Sand Of Vega <sandofvega@gmail.com>
2018-10-20 18:16:19 +0600

    create static tables seeder

---
+++ database/seeds/PackagePropertiesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PackagePropertiesTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('package_properties')->insert([
+            ['name' => 'name'],
+            ['name' => 'description'],
+            ['name' => 'duration'],
+            ['name' => 'fee'],
+            ['name' => 'refer_target'],
+            ['name' => 'refer_onetime_interest'],
+            ['name' => 'refer_renew_interest'],
+            ['name' => 'refer_fail_onetime_interest'],
+            ['name' => 'refer_fail_renew_interest']
+        ]);
+    }
+}
