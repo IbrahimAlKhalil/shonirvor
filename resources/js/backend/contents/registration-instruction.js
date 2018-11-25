2ecb8c442b85e8581a28766089b4dd1a433397c2
IbrahimAlKhalil <hm.ibrahimalkhalil@gmail.com>
2018-11-25 19:14:45 +0600

    Content menagment registration instruction image upload

--- resources/js/backend/contents/registration-instruction.js
+++ resources/js/backend/contents/registration-instruction.js
@@ -8,7 +8,7 @@ import tinymce from '../../../plugins/tinymce/tinymce.min';
 
 tinymce.init({
     selector: '#registration-instruction',
-    plugins: 'advlist colorpicker autoresize legacyoutput media pagebreak toc wordcount anchor hr link save textcolor autolink directionality fullscreen lists noneditable preview searchreplace table textpattern',
+    plugins: 'advlist colorpicker autoresize legacyoutput media pagebreak toc wordcount anchor hr link save textcolor autolink directionality fullscreen lists noneditable preview searchreplace table textpattern image',
     toolbar: 'save undo redo visualaid bold italic underline strikethrough subscript superscript blockformats align formats link openlink preview hr pagebreak searchreplace inserttable tableprops deletetable cell row column toc',
     height: 550,
     branding: false,
