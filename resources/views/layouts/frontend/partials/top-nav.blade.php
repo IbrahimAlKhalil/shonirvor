fb22cafc9f2900095185de4132cbaf7795a46c80
IbrahimAlKhalil <hm.ibrahimalkhalil@gmail.com>
2018-11-25 18:34:59 +0600

    Add favicon icon, logo

--- resources/views/layouts/frontend/partials/top-nav.blade.php
+++ resources/views/layouts/frontend/partials/top-nav.blade.php
@@ -1,5 +1,8 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
+    <a class="navbar-brand" href="{{ route('home') }}">
+        <img src="{{ asset('assets/images/logo.png') }}" style="width: 35px" alt="Logo">
+        {{ config('app.name') }}
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
             aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
